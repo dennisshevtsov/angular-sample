d6908967bbf02152501eccf063d77268c0d05526
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2021-05-15 12:52:57 +0300

    import routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, } from '@angular/core';
 import { BrowserModule, } from '@angular/platform-browser';
+import { Router, } from '@angular/router';
 
 import { AppRoutingModule, } from './app-routing.module';
 import { AppComponent, } from './app.component';
@@ -11,8 +12,8 @@ import { LayoutModule, } from './layout/layout.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     LayoutModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [
@@ -20,4 +21,10 @@ import { LayoutModule, } from './layout/layout.module';
   ]
 })
 export class AppModule {
+  constructor(router: Router) {
+    const replacer = (key: string, value: any): string =>
+      typeof value === 'function' ? value.name : value;
+
+    console.log('Routes: ', JSON.stringify(router.config, replacer, 2));
+  }
 }
