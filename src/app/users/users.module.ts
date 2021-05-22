1b2dc09bf88cbe5591d5279f53dbbd5026e8eeea
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2021-05-23 00:21:22 +0300

    fix build

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,8 +1,10 @@
 import { NgModule,     } from '@angular/core';
 import { CommonModule, } from '@angular/common';
+import { FormsModule,  } from '@angular/forms';
 
-import { UserComponent, } from './components';
-import { UsersRoutingModule } from './users-routing.module';
+import { UserComponent,      } from './components';
+import { UsersRoutingModule, } from './users-routing.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -11,6 +13,8 @@ import { UsersRoutingModule } from './users-routing.module';
   ],
   imports: [
     CommonModule,
+    FormsModule,
+    RouterModule,
   ]
 })
 export class UsersModule {
