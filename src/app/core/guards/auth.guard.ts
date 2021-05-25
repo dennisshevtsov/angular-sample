388402e3632952af292bd8c2ef3a33424ba04741
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2021-05-26 00:36:43 +0300

    auth guard

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,22 +1,44 @@
-import { Injectable,             } from '@angular/core';
-import { ActivatedRouteSnapshot,
-         CanActivate,
-         RouterStateSnapshot,
-         UrlTree,                } from '@angular/router';
+import { Injectable, } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
 
 import { Observable, } from 'rxjs';
+
+import { AuthService, } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  canActivate(
+  public constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) { }
+
+  public canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> |
                                  Promise<boolean | UrlTree> |
                                  boolean | UrlTree {
     console.log('CanActivate Guard is called.');
 
+    const { url } = state;
+
+    return this.checkLogin(url);
+  }
+
+  private checkLogin(url: string): boolean | UrlTree {
+    if (this.authService.isLoggedIn) {
       return true;
+    }
+
+    this.authService.rediredUrl = url;
+
+    return this.router.parseUrl('/login');
   }
 }
