5ae815a9d8d82fbfd9374bd37829c55feb6f944b
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2021-05-27 00:10:42 +0300

    login comp

--- src/app/layout/components/login/login.component.ts
+++ src/app/layout/components/login/login.component.ts
@@ -1,15 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { AuthService } from 'src/app/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
+  public message: string;
 
-  constructor() { }
+  private unsubscribe: Subject<void> = new Subject<void>();
 
-  ngOnInit(): void {
+ public constructor(
+   public authService: AuthService,
+   private router: Router,
+ ) { }
+
+  public ngOnInit(): void {
+    this.setMessage();
   }
 
+  public ngOnDestroy(): void {
+    console.log('[takeUntil ngOnDestroy]');
+
+    this.unsubscribe.complete();
+  }
+
+  public onLogin(): void {
+    this.message = 'Trying to log in ...';
+
+    const observer = {
+      next: () => {
+        this.setMessage();
+
+        if (this.authService.isLoggedIn) {
+          const redirect: string = this.authService.rediredUrl
+            ? this.authService.rediredUrl
+            : '/admin';
+          this.router.navigate([redirect]);
+        }
+      },
+      error: (error: any) => console.log(error),
+      complete: () => console.log('[takeUntil] complete'),
+    };
+
+    this.authService.login()
+                    .pipe(takeUntil(this.unsubscribe))
+                    .subscribe(observer);
+  }
+
+  public onLogout(): void {
+    this.authService.logout();
+    this.setMessage();
+  }
+
+  private setMessage(): void {
+    this.message = 'Logged ' + (this.authService.isLoggedIn ? 'in' : 'out');
+  }
 }
