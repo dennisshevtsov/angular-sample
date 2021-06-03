0bc9f976b9b20ccf887b3d50cb3163854e0c0b2f
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2021-06-03 17:58:27 +0300

    preload strat app comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component, } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
 import { Router, RouterOutlet, } from '@angular/router';
 
-import { MessagesService, } from './core';
+import { CustomPreloadingStrategyService, MessagesService, } from './core';
 import { SpinnerService } from './widgets';
 
 @Component({
@@ -11,14 +11,22 @@ import { SpinnerService } from './widgets';
     './app.component.scss',
   ],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public title: string = 'angular-sample';
 
   public constructor(
     public messagesService: MessagesService,
     public spinnerService: SpinnerService,
+    private preloadingStrategy: CustomPreloadingStrategyService,
     private router: Router,
   ) {}
+
+  public ngOnInit(): void {
+    console.log(
+      'Preloading Modules: ',
+      this.preloadingStrategy.preloadedModules,
+    );
+  }
 
   public onActivate($event: any, routerOutlet: RouterOutlet): void {
     console.log('Activate Component', $event, routerOutlet);
